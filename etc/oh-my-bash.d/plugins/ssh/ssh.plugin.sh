caef2f478c8353df836ecf2727a8da0bf1b07ab8
tschaefer <github@blackox.org>
2024-04-14 13:49:19 +0200

    bash: added method 'ssh-run'

--- etc/oh-my-bash.d/plugins/ssh/ssh.plugin.sh
+++ etc/oh-my-bash.d/plugins/ssh/ssh.plugin.sh
@@ -40,8 +40,33 @@ function _omb_plugin_mosh_tmux {
     mosh "$@" -- tmux -2u new -As0
 }
 
+function _omb_plugin_ssh_run {
+    [ $# -lt 2 ] && return 1
+
+    local host=$1
+    local exec=$2
+    local args=${*:3}
+
+    if ! _omb_util_binary_exists ${exec}; then
+        return 1
+    fi
+
+    exec=$(type -p $exec)
+
+    ssh -t "$host" "
+        export RUN_LIB="'$(mktemp --directory --suffix=.run)'"
+        "'trap "rm -rf $RUN_LIB" EXIT'"
+        echo $'"$(cat $exec | xxd -ps)"' | xxd -ps -r > "'$RUN_LIB/run'"
+        "'chmod +x $RUN_LIB/run'"
+        "'$RUN_LIB/run'" $args
+    "
+
+    return $?
+}
+
 alias ssh-kill='_omb_plugin_ssh_kill'
 alias ssh-list='_omb_plugin_ssh_list'
+alias ssh-run='_omb_plugin_ssh_run'
 alias ssh-config='_omb_plugin_ssh_config'
 alias ssh-screen='_omb_plugin_ssh_screen'
 alias mosh-screen='_omb_plugin_mosh_screen'
