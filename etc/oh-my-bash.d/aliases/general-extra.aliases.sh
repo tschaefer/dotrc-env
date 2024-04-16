16c9ac12a376e6b16d2cce3ee5798345659ccac8
tschaefer <github@blackox.org>
2024-04-16 15:07:10 +0200

    bash: remove unwanted aliases

--- etc/oh-my-bash.d/aliases/general-extra.aliases.sh
+++ etc/oh-my-bash.d/aliases/general-extra.aliases.sh
@@ -5,12 +5,38 @@ alias battery='acpi -b'
 alias ipas='ip --color addr show'
 alias ipr='ip --color route show'
 alias ipl='ip --color link show'
+complete -F _complete_alias ipas
+complete -F _complete_alias ipr
+complete -F _complete_alias ipl
 
 alias gdnp='command git --no-pager diff'
 
 alias vim-notags="vim --cmd 'let g:gutentags_enabled = 0'"
 
 unalias less
+unalias -- -
+unalias ..
+unalias ...
+unalias .3
+unalias .4
+unalias .5
+unalias .6
+unalias 000
+unalias 1
+unalias 2
+unalias 3
+unalias 4
+unalias 5
+unalias 6
+unalias 640
+unalias 644
+unalias 7
+unalias 755
+unalias 775
+unalias 8
+unalias 9
+unalias _
+unalias afind
 
 function _omb_aliases_general_calculator {
     echo "$@" | bc
