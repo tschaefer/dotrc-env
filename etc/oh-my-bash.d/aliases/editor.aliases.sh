645424e8b4f4f7f6b56cb7c0e91db1b009eb8fca
tschaefer <github@blackox.org>
2025-12-25 09:25:45 +0100

    bash: improve editor aliases

--- etc/oh-my-bash.d/aliases/editor.aliases.sh
+++ etc/oh-my-bash.d/aliases/editor.aliases.sh
@@ -1,14 +1,8 @@
 #! bash oh-my-bash.module
 
-case $EDITOR in
-    nano)
-        if [[ -e ${OSH_HOME:-$HOME}/.nanorc ]]; then
-            alias nano="nano --rcfile ${OSH_HOME:-$HOME}/.nanorc"
-        fi
-        ;;
-    vim)
-        if [[ -e ${OSH_HOME:-$HOME}/.vimrc ]]; then
-            alias vim="vim -u ${OSH_HOME:-$HOME}/.vimrc"
-        fi
-        ;;
-esac
+if [[ -e ${OSH_HOME:-$HOME}/.nanorc ]]; then
+    alias nano="nano --rcfile ${OSH_HOME:-$HOME}/.nanorc"
+fi
+if [[ -e ${OSH_HOME:-$HOME}/.vimrc ]]; then
+    alias vim="vim -u ${OSH_HOME:-$HOME}/.vimrc"
+fi
