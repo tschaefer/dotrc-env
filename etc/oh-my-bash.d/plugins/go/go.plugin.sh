ce1a1dcd3dc1a33cea25ebcc0bb7641cf91f9783
tschaefer <github@blackox.org>
2025-12-25 09:30:05 +0100

    bash: enhance go plugin

--- etc/oh-my-bash.d/plugins/go/go.plugin.sh
+++ etc/oh-my-bash.d/plugins/go/go.plugin.sh
@@ -1,6 +1,16 @@
 #! bash oh-my-bash.module
 
-if _omb_util_binary_exists go; then
-    export GOPATH="${OSH_HOME:-$HOME}/.go"
-    export PATH=${OSH_HOME:-$HOME}/.go/bin:${PATH}
+if ! _omb_util_binary_exists go; then
+    return 0
 fi
+
+export GOPATH="${OSH_HOME:-$HOME}/.go"
+export PATH=${OSH_HOME:-$HOME}/.go/bin:${PATH}
+
+function _omb_plugin_go_analyze {
+    [ $# -lt 1 ] && return 1
+
+    go vet -vettool=${GOPATH}/bin/$1 ./...
+}
+
+alias goanalyze="_omb_plugin_go_analyze"
