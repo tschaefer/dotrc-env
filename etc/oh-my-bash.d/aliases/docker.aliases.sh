60aa02e50bb6e36ae5dc5e2d596e225c7e94b54b
tschaefer <github@blackox.org>
2026-01-23 18:47:35 +0100

    bash: add docker ip aliases

--- etc/oh-my-bash.d/aliases/docker.aliases.sh
+++ etc/oh-my-bash.d/aliases/docker.aliases.sh
@@ -1,4 +1,6 @@
 #! bash oh-my-bash.module
 
 alias docls='docker container list --format "table {{.Names}}\t{{.Status}}"'
+alias docipv4='docker container inspect --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"'
+alias docipv6='docker container inspect --format "{{range .NetworkSettings.Networks}}{{.GlobalIPv6Address}}{{end}}"'
 alias doils='docker image list --format "table {{.Repository}}\t{{.Tag}}"'
