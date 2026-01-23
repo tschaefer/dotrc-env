4dc6f1870f2ba0c733c485dc6cb8055a4105c76b
tschaefer <github@blackox.org>
2026-01-23 18:46:09 +0100

    bash: use goenv

--- etc/oh-my-bash.d/plugins/go/go.plugin.sh
+++ etc/oh-my-bash.d/plugins/go/go.plugin.sh
@@ -1,11 +1,18 @@
 #! bash oh-my-bash.module
 
-if ! _omb_util_binary_exists go; then
-    return 0
+if [[ ! -d ${OSH_HOME:-$HOME}/.goenv ]]; then
+    return 1
 fi
 
-export GOPATH="${OSH_HOME:-$HOME}/.go"
-export PATH=${OSH_HOME:-$HOME}/.go/bin:${PATH}
+if [[ ${EUID} == 0 ]]; then
+    export PATH=${OSH_HOME:-$HOME}/.goenv/bin:${PATH}
+else
+    export PATH=${OSH_HOME:-$HOME}/.goenv/bin:${PATH}
+    export GOENV_AUTOMATICALLY_DETECT_VERSION=1
+    export GOENV_GOPATH_PREFIX=${OSH_HOME:-$HOME}/.go
+    eval "$(goenv init -)"
+fi
+
 
 function _omb_plugin_go_analyze {
     [ $# -lt 1 ] && return 1
