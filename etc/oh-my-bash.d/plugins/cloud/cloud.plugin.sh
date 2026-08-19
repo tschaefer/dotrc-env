4ebb83fcbd89735534b40d46e4dfc6adb3c3b6e0
tschaefer <github@blackox.org>
2026-08-19 07:36:04 +0200

    plugin/cloud: cleanup

--- etc/oh-my-bash.d/plugins/cloud/cloud.plugin.sh
+++ etc/oh-my-bash.d/plugins/cloud/cloud.plugin.sh
@@ -1,9 +1,5 @@
 #! bash oh-my-bash.module
 
-if _omb_util_binary_exists aws && _omb_util_binary_exists aws_completer; then
-    complete -C aws_completer aws
-fi
-
 if _omb_util_binary_exists hcloud; then
     source <(hcloud completion bash)
     alias hcloud="hcloud --no-experimental-warnings"
@@ -32,18 +28,6 @@ if _omb_util_binary_exists helm; then
     source <(helm completion bash)
 fi
 
-if _omb_util_binary_exists k3sup; then
-    source <(k3sup completion bash)
-fi
-
-if _omb_util_binary_exists k3d; then
-    source <(k3d completion bash)
-fi
-
-if _omb_util_binary_exists finch; then
-    source <(finch completion bash)
-fi
-
 if _omb_util_binary_exists finchctl; then
     source <(finchctl completion bash)
 fi
@@ -54,6 +38,11 @@ fi
 
 if _omb_util_binary_exists arkade; then
     source <(arkade completion bash)
+fi
+
+if _omb_util_binary_exists argocd; then
+    source <(argocd completion bash)
+    alias argocd="argocd --grpc-web"
 fi
 
 OMB_PLUGIN_CLOUD_FAAS_CLI_GATEWAY=${OMB_PLUGIN_CLOUD_FAAS_CLI_GATEWAY:-"https://f.i.coresec.zone"}
